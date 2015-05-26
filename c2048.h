eb262b23a595d01bc575eaac90eefbdac9788033
zacariaz <zacariaz@gmail.com>
2015-05-27 01:42:44 +0200

    Nothing interesting happened that day...

--- c2048.h
+++ c2048.h
@@ -6,16 +6,16 @@
 #include <stdio.h>
 
 // variable declarations:
-extern const uint8_t* const board[4][4];
-extern uint32_t seed;
-extern const uint32_t* const score;
-extern const int32_t* const moves;
+const uint8_t* const board[4][4];
+uint32_t seed;
+const uint32_t* const score;
+const int32_t* const moves;
 
 // function declaration:
 
-extern void NewGame();
-extern uint8_t Move(uint8_t);
-extern uint8_t SaveGame(const char*);
-extern uint8_t LoadGame(const char*);
+void NewGame();
+uint8_t Move(uint8_t);
+uint8_t SaveGame(const char*);
+uint8_t LoadGame(const char*);
 
 #endif
--- tui.h
+++ tui.h
@@ -1,11 +1,10 @@
 #ifndef TUI_H
 #define TUI_H
 
-#include "c2048.h"
 #include <ncurses.h>
 #include <time.h>
 #include <stdint.h>
 
-extern void Run();
+void Run();
 
 #endif
